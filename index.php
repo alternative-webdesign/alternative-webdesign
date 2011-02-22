d8d06c9180a0821d1f44a3b95d08f1b828a10057
unknown <j.arana@.mission-locale.local>
2011-02-22 09:27:40 +0100

    modification balise title

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
 
 <head>
-	<title>Maquette test</title>
+	<title>Maquette test (Elscorto))</title>
 	<?php include('include/head.php');?>
 	
 	<link rel="stylesheet" media="screen" type="text/css" href="css/style.css" />	
